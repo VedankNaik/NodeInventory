98589e0eabf56333bbc8efa00a0ba867b5d042ee
Vedank022 <vedankn01@gmail.com>
2021-08-18 23:21:07 +0530

    update modal, error fixes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Item = require("./models/items");
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 const mongodb =
   "mongodb+srv://user1:qawsedrf@cluster0.wtf9b.mongodb.net/item-db?retryWrites=true&w=majority";
@@ -15,20 +16,6 @@ mongoose
 
 app.set("view engine", "ejs");
 
-app.get("/create-item", (req, res) => {
-  const item = new Item({
-    name: "Computer",
-    price: 80000,
-  });
-  item.save().then((result) => res.send(result));
-});
-
-app.get("/get-item", (req, res) => {
-  Item.findById("611beb5198627d27602edcc1")
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
 app.get("/", (req, res) => {
   res.redirect("/get-items");
 });
@@ -40,9 +27,8 @@ app.get("/get-items", (req, res) => {
     })
     .catch((err) => console.log(err));
 });
-
-app.get("/add-items", (req, res) => {
-  res.render("add-items");
+app.get("/add-item", (req, res) => {
+  res.render("add-item");
 });
 
 app.post("/items", (req, res) => {
@@ -59,26 +45,23 @@ app.post("/items", (req, res) => {
 app.get("/items/:id", (req, res) => {
   const id = req.params.id;
   Item.findById(id).then((result) => {
-    res.render("item-detail", { items: result });
+    console.log("result", result);
+    res.render("item-detail", { item: result });
   });
 });
 
-app.get("/itemdelete/:id", (req, res) => {
+app.delete("/items/:id", (req, res) => {
   const id = req.params.id;
-  Item.findByIdAndDelete(id)
-    .then((result) => {
-      res.redirect("/get-items");
-    })
-    .catch((err) => console.log(err));
+  Item.findByIdAndDelete(id).then((result) => {
+    res.json({ redirect: "/get-items" });
+  });
 });
 
-app.get("/itemupdate/:id", (req, res) => {
+app.put("/items/:id", (req, res) => {
   const id = req.params.id;
-  Item.findByIdAndUpdate(id, req.body)
-    .then((result) => {
-      res.redirect("/get-items");
-    })
-    .catch((err) => console.log(err));
+  Item.findByIdAndUpdate(id, req.body).then((result) => {
+    res.json({ msg: "Updated Successfully" });
+  });
 });
 
 app.use((req, res) => {
